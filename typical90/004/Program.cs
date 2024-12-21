824dc85ff8119de4827ae3e7afba7ee6ee780c80
t0k0shi <rubisco29@gmail.com>
2024-12-21 18:42:32 +0900

    レベル２の問題を中心に解答

---
+++ typical90/004/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
